a749de5cd9823f1a8dd1d02bc9a3c6a2dc295d2f
rystik <157467592+rystik22@users.noreply.github.com>
2024-07-18 17:41:16 +0400

    Add files via upload

---
+++ mylib/ft_strjoin.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rradin-m <rradin-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 15:44:38 by rradin-m          #+#    #+#             */
+/*   Updated: 2024/07/16 17:40:22 by rradin-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	result = malloc(s1_len + s2_len + 1);
+	if (!result)
+	{
+		return (NULL);
+	}
+	ft_memcpy(result, s1, s1_len);
+	ft_memcpy(result + s1_len, s2, s2_len);
+	result[s1_len + s2_len] = '\0';
+	return (result);
+}
